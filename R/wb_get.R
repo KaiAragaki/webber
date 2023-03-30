1caf14ac70fedc88b25c3eb04ac119cb4b653565
Kai Aragaki <adamaragaki@gmail.com>
2023-03-30 19:06:21 -0400

    get_wb -> wb_get

--- R/wb_get.R
+++ R/wb_get.R
@@ -4,6 +4,9 @@
 #' the specified location, no downloading will occur unless `overwrite = TRUE`.
 #' Will return the local path to the file.
 #'
+#' This is a thin wrapper around `bladdr::get_gbci_file`, and has
+#' hard-coded paths - so it's more a quality-of-life function.
+#'
 #' @param path Character. The path to the image, sans `wb_path`
 #' @param dest_dir Character. The path to save the image.
 #' @param overwrite Logical. Should to file be overwritten if it already exists?
@@ -11,18 +14,18 @@
 #' @param wb_path Character. Path to the top level directory that contains all western blots.
 #' @param quiet Should this function not message upon finding existing files?
 #'
-#' @details This is a thin wrapper around `bladdr::get_gbci_file`, and has
-#' hard-coded paths - so it's more a quality-of-life function.
-#'
 #' @return Character. The local path to the file.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' wb_get("2022-06-22/h3.tif", tempdir(), user = "aragaki-kai", wb_path = "Raw Data/licor")
+#' wb_get(
+#'   "2022-06-22/h3.tif",
+#'   tempdir(),
+#'   user = "aragaki-kai",
+#'   wb_path = "Raw Data/licor"
+#' )
 #' }
-#'
-#'
 wb_get <- function(path,
                    dest_dir,
                    overwrite = FALSE,
@@ -54,11 +57,13 @@ wb_get <- function(path,
 
   e_class <- attr(e, "condition") |> attr("class")
 
-  # TODO: Actually add wb_list
-  if (length(e_class) > 0 && e_class[1] == "http_404") {
-    cli::cli_abort(c(e[1], i = "`wb_list(path)` can help find available blots within a given directory"))
+  if (length(e_class) > 0) {
+    if (e_class[1] == "http_404") {
+      # TODO: Actually add wb_list
+      cli::cli_abort(c(e[1], i = "`wb_list(path)` can help find available blots within a given directory"))
+    }
+    cli::cli_abort(e)
   }
-
   dest_path
 }
 
