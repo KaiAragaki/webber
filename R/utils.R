e5e6683916a25a1c733c60272a50267891a0d5f7
Kai Aragaki <adamaragaki@gmail.com>
2023-11-02 09:35:57 -0400

    refactor: remove dead code

--- R/utils.R
+++ R/utils.R
@@ -1,41 +1,9 @@
-# REFACTOR: 3 times means it's function time
-
-# Seems like some kind of eval substitute magic needs to happen here to get arg
-# names
-
-check_if_user_is_empty <- function(user) {
-  if (user == "") {
-    rlang::abort(
-      c("Argument `user` is blank",
-        i = "This may be because the environmental variable `WEBBER_USR` isn't set, and the `user` argument wasn't supplied.")
-    )
-  }
-}
-
-check_if_wb_path_is_empty <- function(wb_path) {
-  if (wb_path == "") {
-    rlang::abort(
-      c("Argument `wb_path` is blank",
-        i = "This may be because the environmental variable `WEBBER_WBPATH` isn't set, and the `wb_path` argument wasn't supplied.")
-    )
-  }
-}
-
-check_if_wb_edit_path_is_empty <- function(wb_edit_path) {
-  if (wb_edit_path == "") {
-    rlang::abort(
-      c("Argument `wb_edit_path` is blank",
-        i = "This may be because the environmental variable `WEBBER_WBEDITPATH` isn't set, and the `wb_edit_path` argument wasn't supplied.")
-    )
-  }
-}
-
 check_if_arg_is_empty <- function(arg, envar_name) {
   if (arg == "") {
     name <- deparse(substitute(arg))
     cli::cli_abort(
            c("Argument {.code {name}} is blank.",
-             i = "This may be because the environmental variable {.envvar {envar_name}} isn't set and the {.code {name}} argument wasn't supplied")
+             i = "Is envvar {.envvar {envar_name}} set?")
          )
   }
 }
@@ -53,11 +21,11 @@ check_if_wb_path_is_wb_edit_path <- function(wb_path = NULL, wb_edit_path = NULL
   # However, this function isn't about checking to see if something exists
   # The user might not even be supplying one of these args to the parent
   # function.
-  if ((wb_path != "" & wb_edit_path != "") & (wb_path == wb_edit_path)) {
+  if ((wb_path != "" && wb_edit_path != "") && (wb_path == wb_edit_path)) {
     if (wb_path == wb_edit_path) {
       cli::cli_abort(
-        c("wb_path and wb_edit_path (or their environmental variables) are the same!",
-          x = "To protect you from yourself (and overwriting raw data), this is prohibited.")
+        c("wb_path and wb_edit_path (or their envvars) are the same!",
+          x = "To protect you from overwriting raw data, this is prohibited.")
       )
     }
   }
@@ -81,7 +49,7 @@ check_if_bfconvert_exists <- function() {
         i = "bfconvert doesn't appear to be installed on this machine",
         i = "It might not be in your PATH",
         i = "You can download it (and all of bftools) here:",
-        i = "https://bio-formats.readthedocs.io/en/stable/users/comlinetools/index.html"
+        i = "https://bio-formats.readthedocs.io/en/stable/users/comlinetools/index.html" #nolint
       )
     )
   }
